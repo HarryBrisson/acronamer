e21bdeb2cabf274fd641c010d0ba660e1ef4a881
HarryBrisson <ejbrisson@gmail.com>
2019-05-17 19:16:00 -0700

    create placeholder function with get-acronyms endpoint

---
+++ app.py
@@ -0,0 +1,32 @@
+import json
+
+from flask import Flask, request
+
+from acronamer import *
+
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def endpoints():
+    return('try /get-acronyms')
+
+
+@app.route('/get-acronyms')
+def return_acronyms_via_api():
+    try:
+        words = request.args.get('words')
+        order = request.args.get('order')
+        data = get_acronyms(words,order)
+        return json.dumps({'data':data})
+    except Exception as e:
+        return json.dumps({'error':str(e)})
+        
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
