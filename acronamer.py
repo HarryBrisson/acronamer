b121b85ba2f1c7c337867812073942a4cc63082a
HarryBrisson <ejbrisson@gmail.com>
2019-05-19 19:47:28 -0500

    add function to get synonyms for word

--- acronamer.py
+++ acronamer.py
@@ -7,6 +7,14 @@ from nltk.corpus import wordnet as wn
 
 
 # get synonyms for each word
+def get_synonyms(word):
+	synonyms = []
+	synonym_data = wn.synsets(word)
+	for s in synonym_data:
+		synonyms.append(s.lemmas()[0].name())
+	synonyms = list(set(synonyms))
+	return synonyms
+
 
 
 # generate all possible acronyms
